1142c1622bdbc57ca371ec209e9a1dc3f795f7d1
W10-SURFACE-RK <sergio.segura1@ulatina.net>
2018-08-01 20:17:30 -0600

    Try to conect the DB

---
+++ Lab3.Models/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServerTypes.Utilities.LoadNativeAssemblies(AppDomain.CurrentDomain.BaseDirectory);
+
+            var contex = new AdventureWorks2012Entities();
+            Address NewAddress = new Address()
+            {
+                AddressLine1 = "",
+                City = "Home",
+                StateProvinceID = 10,
+                PostalCode = "",
+                ModifiedDate = DateTime.Now,
+                rowguid = Guid.NewGuid()
+        };
+            contex.Addresses.Add(NewAddress);
+            contex.SaveChanges();
+        }
+    }
+}
